206606f64b6e7616bd4e68091dc9814410982010
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-05-04 00:28:18 +0100

    Display data from API

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const list = document.querySelector('.list');
+
 async function fetchIpAddress() {
   const response = await fetch("https://geo.ipify.org/api/v2/country?apiKey=at_m3M1l8QpxvtG9ih8CR732lDeUuxcv");
   const result = await response.json();
@@ -6,6 +8,18 @@ async function fetchIpAddress() {
   // .then((data) => {
   //   console.log(data)
   // })
+  let display = '';
+    display += `
+    <span class="class="scores-span"">${result.ip}</span>
+    <span class="class="scores-span"">${result.location.country}</span>
+    <span class="class="scores-span"">${result.location.region}</span>
+    <span class="class="scores-span"">${result.location.timezone}</span>
+    <span class="class="scores-span"">${result.isp}</span>
+
+    `;
+    list.innerHTML = display;
+    // list.appendChild(scoreInfo);
+
 }
 
 fetchIpAddress();
