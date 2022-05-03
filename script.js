5851b68d6b0cc54763b6003952b6caa76fd34ce5
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-05-04 00:07:39 +0100

    Refactor api function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-async function changeColor() {
-  // 1. Call Scrimba's color API (https://apis.scrimba.com/hexcolors/) to retrieve one random color.
-  // 2. Use that color to update the background color of the body.
-  fetch("https://geo.ipify.org/api/v2/country?apiKey=at_m3M1l8QpxvtG9ih8CR732lDeUuxcv")
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data)
-  })
+async function fetchIpAddress() {
+  const response = await fetch("https://geo.ipify.org/api/v2/country?apiKey=at_m3M1l8QpxvtG9ih8CR732lDeUuxcv");
+  const result = await response.json();
+  console.log(result)
+  // .then(response => response.json())
+  // .then((data) => {
+  //   console.log(data)
+  // })
 }
 
-changeColor();
+fetchIpAddress();
